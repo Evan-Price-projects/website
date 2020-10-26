c117a0f2aef2ee1c24b4e8a7ca71410e9eda1393
Evan Price <evan-price@uiowa.edu>
2020-10-25 20:03:57 -0500

    ADD: adding new stuff

--- db/migrate/20201025230906_create_description.rb
+++ db/migrate/20201025230906_create_description.rb
@@ -2,7 +2,7 @@ class CreateDescription < ActiveRecord::Migration
   def change
     create_table :descriptions do |t|
       t.string 'header'
-      t.text 'description'
+      t.text 'info'
       t.timestamps
     end
   end
