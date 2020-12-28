3ea8c89facedd7a1e5a65a35016a5dd8e68989e9
Evan <eprice1223321@gmail.com>
2020-12-28 17:17:07 -0600

    ADD: adding a table that is formatted better than my best attempts. I used https://www.w3schools.com/Css/tryit.asp?filename=trycss_table_fancy and am not claiming credit for that portion

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,10 @@ descriptions = [
 {project_title: 'Deck of Cards',
  description: 'Working in a simulated agile scrum environment with a team of 5. We went through 3 iterations and used programming tools such as RSPEC, HTML.erb, CSS, Ruby, Rails, Github issues, Pivotal Tracker, 4 weekly scrum meetings, SimpleCov, Sqlite as production and Postgres as development, Heroku Deployment, TDD, BDD, and RESTful services. https://github.com/Evan-Price-projects/CardGame',
  images: '1.png, 2.png, 3.png, 4.png, 5.png, 6.png'},
+{project_title: 'Logical Programming Language',
+ description: 'Created the start to logical programming language similar to prolog using the language Haskell. This language recognizes predicates, queries and commenting out things. It also solves simple one line predicate, query functions.',
+ images: 'haskell1.png'},
 ]
-
 descriptions.each do |description|
   Description.create!(description)
 end
